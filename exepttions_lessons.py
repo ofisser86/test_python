237c807a1b4a0fcf0803dc75a6a8446fa47a07c5
ofisser86 <ofisser86@gmail.com>
2017-11-06 15:41:46 +0100

    some exception implementation

--- exepttions_lessons.py
+++ exepttions_lessons.py
@@ -1,7 +1,41 @@
 def f(x, y):
     try:
         return x / y
-    except Exception:
+    # Use with e and args
+    # except ZeroDivisionError as e :
+    #     print(type(e))
+    #     print(e)
+    #     print(e.args)
+
+    # use with tuple of exceptions
+    except (ZeroDivisionError, TypeError):
         print("Some error here")
 
+
+
 print(f(10,0))
+
+def foo():
+    pass
+
+
+#===================================================
+try:
+    foo()
+except ZeroDivisionError:
+    print("ZeroDivisionError")
+except ArithmeticError:
+    print("ArithmeticError")
+except AssertionError:
+    print("AssertionError")
+#===================================================
+try: foo()
+except (ZeroDivisionError, AssertionError, ArithmeticError ) as e:
+    if isinstance(e, ZeroDivisionError): print('ZeroDivisionError')
+    elif isinstance(e, ArithmeticError): print('ArithmeticError')
+    elif isinstance(e, AssertionError): print('AssertionError')
+#===================================================
+try: foo()
+except (ZeroDivisionError, AssertionError) as e: print(str(type(e))[8:-2])
+except ArithmeticError: print('ArithmeticError')
+
