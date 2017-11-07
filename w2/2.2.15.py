9790889bcc061becd3bc7105d23e310c0304e3ac
ofisser86 <ofisser86@gmail.com>
2017-11-07 16:20:38 +0100

    add techer solution to 2.2.15

--- w2/2.2.15.py
+++ w2/2.2.15.py
@@ -4,3 +4,14 @@ myear, mmonth, mday = input().split(' ')
 days = int(input())
 c = date(int(myear), int(mmonth), int(mday)) + timedelta(days=days)
 print(c.year, c.month, c.day)
+
+# Teacher solution
+import datetime
+
+y, m, d = map(int, input().split())
+days = int(input())
+
+current = datetime.date(year=y, month=m, day=d)
+current += datetime.timedelta(days=days)
+
+print("{} {} {}".format(current.year, current.month, current.day))
