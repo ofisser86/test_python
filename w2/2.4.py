20976874cf9df10d10b0502b91015132309fc12c
ofisser86 <ofisser86@gmail.com>
2017-11-10 13:16:55 +0100

    write to file

--- w2/2.4.py
+++ w2/2.4.py
@@ -1,5 +1,11 @@
-f = open('test.txt')
-for line in f:
-    print(repr(line))
+# f = open('test.txt')
+# for line in f:
+#     print(repr(line))
 
+# f.close()
+# =====================write to file=============
+f = open('test1.txt', 'w')
+line = ['Line1','Line2','Line3']
+content = '\n'.join(line)
+f.write(content)
 f.close()
