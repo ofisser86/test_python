b8ed55a76f61b49184da9b118f60c925672fa163
ofisser86 <ofisser86@gmail.com>
2017-11-14 17:20:18 +0100

    again json

--- w3/3.5.3_json.py
+++ w3/3.5.3_json.py
@@ -8,4 +8,10 @@ student = {
   }
 
 data = [student]
-print(json.dumps(data, indent=4, sort_keys=True))
+#print(json.dumps(data, indent=4, sort_keys=True))
+# with open('dump.json', 'w') as f:
+#     json.dump(data, f, indent=4, sort_keys=True)
+
+data_json = json.dumps(data, indent=4, sort_keys=True)
+data_again = json.loads(data_json)
+print(sum(data_again[0]['mark']))
