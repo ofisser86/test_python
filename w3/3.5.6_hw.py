39afd3f17abe5c91cd30722fc5fc1026a87526e2
ofisser86 <ofisser86@gmail.com>
2017-11-15 14:32:07 +0100

    requests authors ans

--- w3/3.5.6_hw.py
+++ w3/3.5.6_hw.py
@@ -17,3 +17,23 @@ if c == 0:
     print("No")
 else:
     print("Yes")
+
+# =====================Authors answers ======================
+import re
+import requests
+
+start_url = input()
+end_url = input()
+
+found = False
+
+link_pattern = re.compile(r'<a[^>]*?href="(.*?)"[^>]*?>')
+
+resp = requests.get(start_url).text
+for url in link_pattern.findall(resp):
+    cur_resp = requests.get(url).text
+    if end_url in link_pattern.findall(cur_resp):
+        found = True
+        break
+
+print("Yes" if found else "No")
