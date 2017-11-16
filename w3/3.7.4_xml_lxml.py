029b2a15d4c133f5f263d48fc729add793cf1bb5
ofisser86 <ofisser86@gmail.com>
2017-11-16 15:01:40 +0100

    use write method

--- w3/3.7.4_xml_lxml.py
+++ w3/3.7.4_xml_lxml.py
@@ -3,5 +3,18 @@ from xml.etree import ElementTree
 tree = ElementTree.parse('example.xml')
 root = tree.getroot()
 
-print(tree)
-print(root, root.tag, root.attrib)
+# print(tree)
+# print(root, root.tag, root.attrib)
+
+# for child in root:
+#     print(child.tag)
+#     for sub in child:
+#         print(sub.text)
+
+#print(root[0][0].text)
+# for element in root.iter("score"):
+#     for child in element:
+#         print(child.text)
+#     print(element)
+
+tree.write('example_copy.xml')
