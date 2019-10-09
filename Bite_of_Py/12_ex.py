eeb07592f4983638e5fbcfc1d636a28d2260fcdf
Didi <ofisser86@gmail.com>
2019-10-09 17:27:55 +0300

    Add files to Pytutor

---
+++ Bite_of_Py/12_ex.py
@@ -0,0 +1,8 @@
+a = ['banana', 'apple', 'mango']
+b = a
+del b[1]
+print(a)
+c = {1, 2, 4, 7, 4, 1, 2}
+n = set([1, 2, 4, 7, 4, 1, 2])
+print(type(c))
+print(type(n))
---
+++ pytutor/4-4.py
@@ -0,0 +1,8 @@
+i = 0
+sum = 0
+while i < 10:
+    a = int(input())
+    sum += a
+    i += 1
+
+print(sum)
