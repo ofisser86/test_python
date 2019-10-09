28ce4d1d8e6f5a3d3c787b23350cccb23991d0ad
Didi <ofisser86@gmail.com>
2019-10-09 17:26:23 +0300

    Hamming tastks

--- exercism/python/hamming/hamming.py
+++ exercism/python/hamming/hamming.py
@@ -1,2 +1,12 @@
 def distance(strand_a, strand_b):
-    pass
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Different Hamming type")
+    differ = 0
+    for i in range(len(strand_a)):
+        if strand_a[i] != strand_b[i]:
+            differ += 1
+
+    return differ
+
+
+#distance('GAGCCTACTAACGGGAT', 'CATCGTAATGACGGCC')
