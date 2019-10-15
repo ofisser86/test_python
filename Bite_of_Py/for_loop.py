a645c322424572b964c67e79e6582a2b1cf4b255
Didi <ofisser86@gmail.com>
2019-10-15 16:09:09 +0300

    Add for_loop tasks

---
+++ Bite_of_Py/for_loop.py
@@ -0,0 +1,9 @@
+children = ["dima_1986", "maria_1987", "taras_2000", "olya_1999"]
+
+child_names = []
+for child in children:
+    # divide names and year
+    child_part = child.split("_")[0].title()
+    child_names.append(child_part)
+
+print(" ".join(child_names))
